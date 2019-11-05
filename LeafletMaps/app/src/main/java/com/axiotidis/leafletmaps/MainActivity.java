5c9de8c13dc232473af2c36cb109322f0b5f43ea
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-11-05 23:09:37 +0200

    first commit

---
+++ LeafletMaps/app/src/main/java/com/axiotidis/leafletmaps/MainActivity.java
@@ -0,0 +1,69 @@
+package com.axiotidis.leafletmaps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    /**
+     * WebViewClient subclass loads all hyperlinks in the existing WebView
+     */
+    public class GeoWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // When user clicks a hyperlink, load in the existing WebView
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    /**
+     * WebChromeClient subclass handles UI-related calls
+     * Note: think chrome as in decoration, not the Chrome browser
+     */
+    public class GeoWebChromeClient extends WebChromeClient {
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin,
+                                                       GeolocationPermissions.Callback callback) {
+            // Always grant permission since the app itself requires location
+            // permission and the user has therefore already granted it
+            callback.invoke(origin, true, false);
+        }
+    }
+
+    WebView mWebView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webView1);
+        // Brower niceties -- pinch / zoom, follow links in place
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.setWebViewClient(new GeoWebViewClient());
+        // Below required for geolocation
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setGeolocationEnabled(true);
+        mWebView.setWebChromeClient(new GeoWebChromeClient());
+        // Load google.com
+        mWebView.loadUrl("https://axiotidis.github.io/map/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Pop the browser back stack or exit the activity
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
